2afcb0a4b1ea41fd8de9bdde29370f984dad47ba
Nero <nero@example.com>
2026-02-14 22:23:36 -0300

    Fix missing closing tag in About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -192,6 +192,7 @@ export function About() {
                                     sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 600px"
                                 />
                             </div>
+                        </div>
                     </m.div>
 
                     {/* Content Side */}
