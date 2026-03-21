4a2410ab47e1f738293ae5108c2d1fcf88d4fc0c
Nero <nero@example.com>
2026-03-21 00:11:58 -0300

    style(hero): move video position to the extreme right

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -103,7 +103,7 @@ export function Hero() {
                     width: 100%;
                     height: 100%;
                     object-fit: cover;
-                    object-position: 80% center;
+                    object-position: right center;
                     filter: grayscale(100%) brightness(0.7) contrast(1.4);
                     z-index: 0;
                     pointer-events: none;
